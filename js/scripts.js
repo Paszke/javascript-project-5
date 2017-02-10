e2ba33e086dc55e91639b7e4bed609e64e839c97
Paweł Krzyśko <pawelkrzysko@gmail.com>
2017-02-10 17:48:13 +0100

    correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // plik scripts.js
 
-function rysujchoinkę(b) {
-	for ( var n = 0 ; n < 6 ; n++) {
-		var star = " ";
-		for ( var a = 0 ; a*2-1 ; a++) {
-			var star = (star += '*');
+function rysujChoinke(b) {
+	for ( var n = 0 ; n <= b ; n++) {
+		var star = '';
+		for ( var a = 0 ; a <= n * 2 - 1 ; a++) {
+			star += '*';
 		}
 		console.log(star);
 	}
 }
 
+rysujChoinke(5);
